6e0a7241a9ca151ad1afc715a813703ba924c102
DungK5UIT <nguyenhuudung1072005@gmail.com>
2025-10-09 20:43:24 +0700

    hoàn thiện dự án

--- MakeGreen/src/main/java/com/project/MakeGreen/repositories/TramXeRepository.java
+++ MakeGreen/src/main/java/com/project/MakeGreen/repositories/TramXeRepository.java
@@ -1,15 +1,17 @@
 package com.project.MakeGreen.repositories;
 
 import com.project.MakeGreen.models.TramXe;
+import com.project.MakeGreen.models.TramXeId; // <-- Import class TramXeId
 import com.project.MakeGreen.models.Xe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
+// import java.util.UUID; // Dòng này không cần thiết nữa
 
 @Repository
-public interface TramXeRepository extends JpaRepository<TramXe, UUID> {
+// Thay đổi ở đây: từ UUID -> TramXeId
+public interface TramXeRepository extends JpaRepository<TramXe, TramXeId> {
     
     // Phương thức để tìm liên kết Tram-Xe dựa trên đối tượng Xe
     Optional<TramXe> findByXe(Xe xe);
